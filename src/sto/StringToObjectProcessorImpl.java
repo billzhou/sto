f3690eaaa6bf50df398d2ac61601097a1ec28453
zhogu001 <zhogu001@localhost>
2017-04-07 23:12:24 -0400

    test

--- src/sto/StringToObjectProcessorImpl.java
+++ src/sto/StringToObjectProcessorImpl.java
@@ -1,14 +1,17 @@
 package sto;
 
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Map;
 
+import sto.builder.PositionIndexer;
 import sto.builder.StringToObjectBuilder;
 import sto.builder.StringToObjectBuilderResult;
 import sto.objects.MessageObjectContainer;
 import sto.objects.TestDetail;
 import sto.objects.TestHeader;
-
+//
 public class StringToObjectProcessorImpl {
 	
 	private static String testString = "header  2detail1detail2";
@@ -34,7 +37,7 @@ public class StringToObjectProcessorImpl {
 //		return c;		
 //	}
 //	
-	public static MessageObjectContainer createObjectContainerWithResult(String message) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InstantiationException, ClassNotFoundException{
+	public static MessageObjectContainer createObjectContainerWithResult(String message) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InstantiationException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException{
 		MessageObjectContainer container = new MessageObjectContainer();
 		Integer index = 0;
 		
@@ -55,26 +58,42 @@ public class StringToObjectProcessorImpl {
 		return container;		
 	}
 	
+	public static MessageObjectContainer createObjectContainer(String message) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InstantiationException, ClassNotFoundException, InvocationTargetException, IntrospectionException, NoSuchMethodException{
+		MessageObjectContainer container = new MessageObjectContainer();
+		PositionIndexer positionIndexer = new PositionIndexer();
+		
+		TestHeader header = StringToObjectBuilder.buildObject(TestHeader.class, testString,positionIndexer);
+		container.header = header;
+		
+		int count = header.getCount();
+		for(int i = 0; i < count; i++){
+			TestDetail detail = StringToObjectBuilder.buildObject(TestDetail.class, testString,positionIndexer);
+			container.getDetails().add(detail);
+		}
+		
+		return container;		
+	}
+	
 	public static void main(String[] args){
 		try {
-//			MessageObjectContainer t = StringToObjectProcessorImpl.createObjectContainer(testString);
-//			System.out.println(t.getHeader().header);
-//			System.out.println(t.getHeader().count);
-//			
-//			List<TestDetail> ds = t.getDetails();
-//			for(TestDetail d:ds){
-//				System.out.println(d.detail);
-//			}
+			MessageObjectContainer t = StringToObjectProcessorImpl.createObjectContainer(testString);
+			System.out.println(t.getHeader().header);
+			System.out.println(t.getHeader().count);
 			
-			
-			MessageObjectContainer tt = StringToObjectProcessorImpl.createObjectContainerWithResult(testString);
-			System.out.println(tt.getHeader().header);
-			System.out.println(tt.getHeader().count);
-			
-			List<TestDetail> dst = tt.getDetails();
-			for(TestDetail d:dst){
+			List<TestDetail> ds = t.getDetails();
+			for(TestDetail d:ds){
 				System.out.println(d.detail);
 			}
+			
+			
+//			MessageObjectContainer tt = StringToObjectProcessorImpl.createObjectContainerWithResult(testString);
+//			System.out.println(tt.getHeader().header);
+//			System.out.println(tt.getHeader().count);
+//			
+//			List<TestDetail> dst = tt.getDetails();
+//			for(TestDetail d:dst){
+//				System.out.println(d.detail);
+//			}
 		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -91,6 +110,15 @@ public class StringToObjectProcessorImpl {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IntrospectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
